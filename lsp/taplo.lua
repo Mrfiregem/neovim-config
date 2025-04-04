770634f82dc96b170fae7aeed4557af8a29458ba
Firegem <firinggems@gmail.com>
2025-04-04 03:46:13 -0400

    Fix "this document has been excluded" warning from taplo lsp

--- lsp/taplo.lua
+++ lsp/taplo.lua
@@ -1,6 +1,9 @@
+local lsputil = require "utils.lsp"
+
 return {
     cmd = { "taplo", "lsp", "stdio" },
     filetypes = { "toml" },
-    root_markers = { ".git" },
+    root_dir = lsputil.root_globs { ".git", "*.toml" },
+    -- root_markers = { ".git" },
     single_file_support = true,
 }

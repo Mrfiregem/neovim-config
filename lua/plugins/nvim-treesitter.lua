d06368b311ff7e8cffe7d3f219bc1ebf6ef8e68b
Firegem <firinggems@gmail.com>
2025-04-07 22:37:48 -0400

    set nvim-treesitter.ensure_installed from /lua/config/ts-list.txt file

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,25 @@
-local parsers = { "nu", "nim", "html", "css", "javascript", "php", "python" }
+---@return string[]
+local read_tslist_file = function()
+    local path = vim.fs.joinpath(vim.fn.stdpath "config", "lua", "config", "ts-list.txt")
+    local f = io.open(path, "r")
+    if not f then return {} end
+    local parsers = {}
+    for p in f:lines() do
+        if p ~= "" then parsers[#parsers + 1] = p end
+    end
+    f:close()
+    return parsers
+end
+
 local M = {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     lazy = false,
     config = function()
         local configs = require "nvim-treesitter.configs"
+        ---@diagnostic disable-next-line: missing-fields
         configs.setup {
-            ensure_installed = parsers,
+            ensure_installed = read_tslist_file(),
             highlight = { enable = true },
             indent = { enable = true },
         }
